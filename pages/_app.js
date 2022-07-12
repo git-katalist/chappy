6cc2e6463254ba004b2ec76f87d5bd1f8111de55
tsots <mosalatsotetsi@gmail.com>
2022-07-12 18:41:32 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import { Theme } from "../styles/Theme";
+import { Global } from "styles/Global";
+import Nav from "@Components/navigation/Nav";
+import Footer from "@Components/footer/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={Theme}>
+      <Global />
+      <Nav/>
+       <Component {...pageProps} />
+      <Footer/>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
